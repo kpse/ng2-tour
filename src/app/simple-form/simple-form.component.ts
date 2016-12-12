49461dd058f5f5306d2f21bde504207b1a0c0191
Suo Qin <kpsesuo@gmail.com>
2016-12-12 19:28:04 +1100

    click event

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -4,13 +4,14 @@ import {Component, OnInit} from "@angular/core";
   selector: 'app-simple-form',
   template: `<div>
     <input #myInput type="text">
-    <button (click)="onClick(myInput.value)">Click me</button>
+    <button (click)="onClick($event, myInput.value)">Click me</button>
 </div>`,
   styles: []
 })
 export class SimpleFormComponent implements OnInit {
 
-  onClick(value) {
+  onClick(event, value) {
+    console.log('clicked event:', event);
     console.log('clicked:', value);
   }
 

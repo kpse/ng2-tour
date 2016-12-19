b6e441d988161152b7df5b92c9455bd4f0ba99ec
Suo Qin <kpsesuo@gmail.com>
2016-12-19 18:14:48 +1100

    use ngClass

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -3,7 +3,14 @@ import {Component, OnInit, Input, Output, EventEmitter} from "@angular/core";
 @Component({
   selector: 'app-simple-form',
   template: `<div>
-    <input #myInput type="text" [(ngModel)]="message">
+    <input #myInput 
+    type="text" 
+    [(ngModel)]="message"
+    [ngClass]="{mousedown:isMouseDown}"
+    (mousedown)="isMouseDown = true"
+    (mouseup)="isMouseDown = false"
+    (mouseleave)="isMouseDown = false"
+    >
     <button (click)="update.emit({text:message})">Click me</button>
 </div>`,
   styles: [`
@@ -22,10 +29,16 @@ outline: none;
 button {
 border: none;
 }
+
+.mousedown {
+border: 2px solid green;
+}
+
 `]
 })
 export class SimpleFormComponent implements OnInit {
 
+  isMouseDown;
   @Input() message;
 
   @Output() update = new EventEmitter();

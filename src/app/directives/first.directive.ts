9b2c391e235cb5dd795e960cea08e78f0b91f383
Suo Qin <kpsesuo@gmail.com>
2017-01-07 09:11:41 +1100

    add host listener

--- src/app/directives/first.directive.ts
+++ src/app/directives/first.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, HostBinding, Input} from '@angular/core';
+import {Directive, HostBinding, Input, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[first]'
@@ -8,6 +8,11 @@ export class FirstDirective {
   @Input() first;
   @HostBinding() get innerText() {
     return this.first;
+  }
+
+  @HostListener('click', ['$event']) onClick(event) {
+    console.log(`on clicking in directive event: ${event}`);
+    this.first = event;
   }
 
   constructor() { }

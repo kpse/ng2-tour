69d0bd98921e2fd0344bd5b5e79993d4615dba84
Suo Qin <kpsesuo@gmail.com>
2017-01-09 00:31:40 +1100

    host binding on `disabled` is broken

--- src/app/directives/online.directive.ts
+++ src/app/directives/online.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, HostBinding} from '@angular/core';
+import {Directive, HostBinding} from "@angular/core";
 import {OnlineService} from "../services/online.service";
 
 @Directive({
@@ -6,9 +6,9 @@ import {OnlineService} from "../services/online.service";
 })
 export class OnlineDirective {
 
-  @HostBinding('disable') get disabled() {
-    return this.service.online;
-  }
+  // @HostBinding('disabled') get disabled() {
+  //   return this.service.online;
+  // }
 
   @HostBinding('class.offline') get offline() {
     return this.service.online;

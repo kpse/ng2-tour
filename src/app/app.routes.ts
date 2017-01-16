73bc6d8db4fc8de0c50444dc1df2af91369ab432
Louis Suo <louis.suo@myob.com>
2017-01-16 12:07:10 +1100

    use route redirect & update packages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import {RouterModule} from "@angular/router";
 import {HeroesComponent} from "./heroes/heroes.component";
-import {HomeComponent} from "./home/home.component";
 import {CrisisCenterComponent} from "./crisis-center/crisis-center.component";
 
 export const routes = [
-  {path: "", component: HomeComponent},
-  // {path: "crisis-center", loadChildren: 'app/crisis-center/crisis-center.module#CrisisCenterModule'},
+  {path: "", redirectTo: 'heroes', pathMatch: 'full'},
+  // {path: "crisis-center", loadChildren: 'app/crisis-center/crisis-center.module'},
   {path: "crisis-center", component: CrisisCenterComponent},
   {path: "heroes", component: HeroesComponent}
 ]
